6b98b799770d99f80fb6643f260490167421f39c
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-04-03 07:58:15 +0500

    Добавьте файлы проекта.

---
+++ GITA_exam_8masala/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Summani kiriting: ");
+float E,S=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Foizni kiriting: ");
+float B=0,p=Convert.ToInt32(Console.ReadLine());
+E = 2 * S;
+int cout = 0;
+while (E>B)
+{   cout++;
+    S = S * ((100 + p) / 100);
+    B = S;
+}
+Console.WriteLine(Convert.ToString(cout) + " Oyda " + Convert.ToString(B) + " so'm" );
+
